07750c12ba852d09cc0e49857e78397f0304d6c7
Jaizu <jaizu85@gmail.com>
2022-02-27 14:26:55 +0100

    Shops now set the obtained item flag

--- include/item.h
+++ include/item.h
@@ -80,6 +80,7 @@ u8 ItemId_GetSecondaryId(u16 itemId);
 void DeserializeTmHmItemSlots(void);
 void DrawHeaderBox(void);
 void HideHeaderBox(void);
+bool8 GetSetItemObtained(u16 item, u8 caseId);
 
 enum ItemObtainFlags
 {
