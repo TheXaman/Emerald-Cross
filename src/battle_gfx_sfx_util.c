116bd6f7f57fa2bb662d868c481e596c09905371
Jaizu <jaizu85@gmail.com>
2022-03-06 14:19:32 +0100

    Revert SpriteCB_TrainerSlideIn

--- src/battle_gfx_sfx_util.c
+++ src/battle_gfx_sfx_util.c
@@ -372,20 +372,9 @@ void SpriteCB_TrainerSlideIn(struct Sprite *sprite)
 {
     if (!(gIntroSlideFlags & 1))
     {
-        if (gSaveBlock2Ptr->optionsSkipBattleIntro == 0)
+        sprite->x2 += sprite->sSpeedX;
+        if (sprite->x2 == 0)
         {
-            sprite->x2 += sprite->sSpeedX;
-            if (sprite->x2 == 0)
-            {
-                if (sprite->y2 != 0)
-                    sprite->callback = SpriteCB_TrainerSlideVertical;
-                else
-                    sprite->callback = SpriteCallbackDummy;
-            }
-        }
-        else
-        {
-            sprite->x2 = 0;
             if (sprite->y2 != 0)
                 sprite->callback = SpriteCB_TrainerSlideVertical;
             else
