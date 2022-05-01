68313b73e34f2803c6652a55d30c15162ff0f316
Jaizu <jaizu85@gmail.com>
2022-05-01 12:30:05 +0200

    Surfing Dismount Ground Effects

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -1681,6 +1681,7 @@ static void Task_WaitStopSurfing(u8 taskId)
         gPlayerAvatar.preventStep = FALSE;
         ScriptContext2_Disable();
         DestroySprite(&gSprites[playerObjEvent->fieldEffectSpriteId]);
+        playerObjEvent->triggerGroundEffectsOnMove = TRUE;
         DestroyTask(taskId);
     }
 }
